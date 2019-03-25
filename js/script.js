276a1129c99755647607aeed3af9bac07462189b
Santa Claus <santa@northpole.com>
2019-03-25 09:04:49 +0300

    alert changes on script.js

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@ function mefun() {
  var no3=parseFloat(gain3);
 
  if (no1<=0){
-  alert("Side one cannot be less than zero.Can't be a triangle")
+  alert("It can't be a triangle if face one is less than zero." )
 }else if (no2<=0) {
-  alert("Side two cannot be less than zero.Can't be a triangle")
+  alert("It can't be a triangle if face two is less than zero.")
 }else if(no3<=0) {
-   alert("side three cannot be less than zero.Can't be a triangle")
+   alert("It can't be a triangle if face three is less than zero.")
  }else if(no1===no2 && no1===no3) {
 alert("Equilateral")
 } else if(no1===no2||no2===no3){
